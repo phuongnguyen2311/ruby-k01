6abf9a5ec229d083acf6810915cda24399ecabb1
cafedevn <tranhuuthangcoi@gmail.com>
2021-07-25 11:16:37 +0700

    add bt8

---
+++ bt8/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    before_save :downcase_email
+    validates :name, presence: true
+    validates :email, presence: true,uniqueness: true,length: { maximum: 50,minimum: 2 },format: { with: URI::MailTo::EMAIL_REGEXP } 
+    def downcase_email
+        self.email.downcase!
+    end
+    has_secure_password
+end
