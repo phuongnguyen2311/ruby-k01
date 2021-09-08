70085dfd7cb35ed962e6bbca163f4458ef7f76ce
congiap94 <conggiap1308@gmail.com>
2021-09-08 19:07:36 +0700

    devise and social login

---
+++ devise/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users,
+  path: '',
+  path_names: {sign_in: 'login' ,sign_out: 'logout' ,edit: 'profile',sign_up: 'register'},
+  controllers: {omniauth_callbacks: 'omniauth_callbacks' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
